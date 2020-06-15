bfa03248b9f9969db0d0fc0279331bd9ce50b853
shubham2050 <50189119+shubham2050@users.noreply.github.com>
2020-06-15 22:57:19 +0530

    Update main.py
    Registration and login fields (+updates)

--- main.py
+++ main.py
@@ -29,6 +29,27 @@ class LoginForm(Form):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+    
+#This API is for registration validation works fine with registration.html
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+#this is for login
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 app = Flask(__name__)
 
