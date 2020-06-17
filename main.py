1907396e6a0d1d610f1f260d33fdec76845a3b1b
shubham kumar tiwari <shbhmtwr19@gmail.com>
2020-06-17 20:04:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,22 +22,48 @@ app.run(debug=True)
 #---------------------------------------------
 
 
+
+
+class StatementDateForm(Form):
+    
+    start_date = DateField(' Start date', validators=[DataRequired()])
+    end_date = DateField(' End date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class StatementNumberForm(Form):
+    
+    number = IntegerField('number', validators=[DataRequired(), Length(min=1, max=30)])
+    submit = SubmitField('Submit')
+
+class TransferForm(Form):
+    
+    said = StringField(' scource Account Id', validators=[DataRequired()])
+    taid = StringField(' target Account Id', validators=[DataRequired()])
+    amount = IntegerField('Amount', validators=[DataRequired(), Length(min=1, max=2)])
+    submit = SubmitField('Submit')
+
+class DepositForm(Form):
+    
+    aid = StringField('Account Id', validators=[DataRequired()])
+    amount = IntegerField('Amount', validators=[DataRequired(), Length(min=1, max=2)])
+    submit = SubmitField('Submit')
+
+class CreateAccountForm(Form):
+    
+    cid = StringField('Customer Id', validators=[DataRequired()])
+    atype= StringField('Account type',validators=[DataRequired(), Length(min=2, max=20)])
+    deposit = IntegerField('Deposit', validators=[DataRequired(), Length(min=1, max=2)])
+    submit = SubmitField('Submit')
+
 class RegistrationForm(Form):
-    username = StringField('Customer Name',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Customer SSM Id',
-                        validators=[DataRequired()])
-    age = IntegerField('Age',
-                           validators=[DataRequired(), Length(min=1, max=2)])
-    address = StringField('Address',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    state = StringField('State',
-                           validators=[DataRequired(), Length(min=2, max=20)])
-    city = StringField('City',
-                           validators=[DataRequired(), Length(min=2, max=20)])
+    name = StringField('Customer Name',validators=[DataRequired(), Length(min=2, max=20)])
+    ssnid = StringField('Customer SSM Id', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired(), Length(min=1, max=2)])
+    address = StringField('Address', validators=[DataRequired(), Length(min=2, max=20)])
+    state = StringField('State', validators=[DataRequired(), Length(min=2, max=20)])
+    city = StringField('City', validators=[DataRequired(), Length(min=2, max=20)])
     password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm Password',
-                                     validators=[DataRequired(), EqualTo('password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Submit')
     #reset = ResetField('Reset')
 
